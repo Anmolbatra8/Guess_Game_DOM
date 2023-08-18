77db780f87708f064305c02ca31ee423e0446806
Anmolbatra8 <115956152+Anmolbatra8@users.noreply.github.com>
2023-08-18 17:21:46 +0530

    Update Guess_game_W2.js
    Updating JavaScript Functionality of the Game and Correcting some mistakes (18AUG'2023)

--- Guess_game_W2.js
+++ Guess_game_W2.js
@@ -1,82 +1,97 @@
+// 1)Selecting Essential DOM Elements : 
+
 'use strict';
-var again = document.querySelector('.again');
-var number = document.querySelector('.number');
-var guess = document.querySelector('.guess');
-var check = document.querySelector('.check');
-var msg = document.querySelector('.message');
-var score = document.querySelector('.score');
-var highscore = document.querySelector('.highscore');
-let randomnumber = Math.trunc(Math.random() * 20) + 1;
-console.log(randomnumber);
-let scores = 20;
-let checkno = () => {
-  const guessno = Number(guess.value);
-  console.log(guessno);
-  if (!guess) {
-    msg.textContent = 'no number';
-  } 
-  
-  else if (guessno === randomnumber) {
-    msg.textContent = "correct number";
-    number.textContent = randomnumber;
+var guessElement = document.querySelector(".guess");
+//selecting the input field for user's guesses
+var guessValue = Number(guessElement.value);
+//Value which the user will eneter in the input field
+
+var msgElement = document.querySelector(".message");
+var randomnumber = Math.trunc(Math.random() * 20) + 1;
+
+//Check and Again button
+var checkButtonElement = document.querySelector(".check");
+var againButtonElement = document.querySelector(".again");
+
+var score = document.querySelector(".score");
+var highscore = document.querySelector(".highscore");
+var displayqumark = document.querySelector(".number");
+
+// 2)FUNCTIONS AND OTHER NEEDED values
+let scoreValue = 20;
+//initial value to be set when game starts
+let highscoreValue = 0;
+
+//To CHECK RANDOM WITH GUESS VALUE:
+
+function check() {
+console.log(randomnumber,typeof randomnumber);
+   msgElement.textContent = "Larger";
+   //When User Guess a Larger value than Generated:
+
+   if(randomnumber < Number(guessElement.value)) {
+    msgElement.textContent = "Go Small Buddy!"
+    document.querySelector("body").style.backgroundColor = "red";
+    scoreValue--;
+    console.log("score reduced",scoreValue);
+    score.textContent = scoreValue;
+    displayqumark.textContent = ">";
+   }
+   //When User Guess is Smaller than the Number Generated: 
+
+   else if (Number(guessElement.value)< randomnumber) {
+    msgElement.textContent = "Go Large Buddy!"
+    document.querySelector("body").style.backgroundColor = "#AA336A";
+    scoreValue--;
+    console.log("score reduced",scoreValue);
+    score.textContent = scoreValue;
+    displayqumark.textContent = "<";
+   }
+    
+   // When User Guessed Correctly:
+   else{ 
+    msgElement.textContent = "You Did It !"
     document.querySelector("body").style.backgroundColor = "green";
-    number.style.width = "30rem";
-    if (scores > highscore.textContent) {
-      highscore.textContent = scores;
-    }
-
-  }
-
-
+    
+    console.log("score: ",scoreValue);
+    score.textContent = scoreValue;
+    displayqumark.textContent = randomnumber;
+    displayqumark.style.backgroundColor = "#FFD93D";
+    displayqumark.style.width = "30rem";
 
 
-  /* else if (guessno === randomnumber) {
-    msg.textContent = 'correct number';
-    highscore.textContent = scores;
-    number.textContent = randomnumber;
-    document.querySelector('body').style.backgroundColor = 'green';
-    number.style.width = '30rem';
-    console.log(typeof(scores),typeof(Number(highscore.textContent)));
-    let a = Number(highscore.textContent);
-    console.log(a,typeof(a));
+    //Updating High Score (When user Has Won the Game)
+    if(scoreValue > highscoreValue) {
+        highscore.textContent = scoreValue; 
+       }
 
-    if (scores < a) {
-        console.log(a,"value");
-      highscore.textContent = scores;
-    }
-  } 
-  
-  */
+   }
+   }
 
-  else if (guessno > randomnumber) {
-    if (scores > 0) {
-      msg.textContent = 'too high !';
-      scores--;
-      console.log('k' + scores);
-      score.textContent = scores;
-    } else {
-      msg.textContent = 'you lost the game!';
-    }
-  } else if (guessno < randomnumber) {
-    if (scores > 0) {
-      msg.textContent = 'too low !';
-      scores--;
-      console.log('k' + scores);
-      score.textContent = scores;
-    } else {
-      msg.textContent = 'you lost the game!';
-    }
-  }
-};
-check.addEventListener('click', checkno);
-var resetgame = () => {
-  document.querySelector('body').style.backgroundColor = '#222';
-  number.style.width = '15rem';
-  scores = 20;
-  score.textContent = scores;
-  msg.textContent = 'Start guessing...';
-  number.textContent = '?';
-  randomnumber = Math.trunc(Math.random() * 20) + 1;
-  console.log('ra', randomnumber);
-};
-again.addEventListener('click', resetgame);
+  //When Click On "Again " Button , The game resets and New random Number is generated
+
+
+
+   function reset() {
+    //generate a new random Number
+    randomnumber = Math.trunc(Math.random() * 20) + 1;
+    displayqumark.textContent = "?";
+    displayqumark.style.width = "15rem";
+    msgElement.textContent  ="Start Guessing...";
+    document.querySelector("body").style.backgroundColor = "#222";
+    guessElement.value="";
+    scoreValue = 20;
+    score.textContent= scoreValue ;
+   }
+
+
+//3)EVENT LISTENERS
+
+//check () when "Check Button" is Clicked
+checkButtonElement.addEventListener("click",check);
+
+//reset() when "Again button" is clicked
+againButtonElement.addEventListener('click',reset);
+
+
+//CRAFTED WITH GUIDANCE OF MENTORS AT GFG NOIDA (Mern-3 batch)
